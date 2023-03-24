432e03e46a8a8df30bee6d541e07e34366de3a47
hafezfhmi <hafezfahmi2000@gmail.com>
2023-03-24 14:22:33 +0800

    b.027 Destructuring with Annotations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -29,3 +29,21 @@ const throwError2 = (message: string): string => {
 
   return message;
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
