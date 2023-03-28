930779c421407a75f4e875f22b0d87d44ea26ec5
hafezfhmi <hafezfahmi2000@gmail.com>
2023-03-28 16:07:56 +0800

    b.071 Optional Implements Clauses

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -22786,6 +22786,7 @@ exports.User = void 0;
 var faker_1 = require("@faker-js/faker");
 var User = /** @class */function () {
   function User() {
+    this.color = "red";
     this.name = faker_1.faker.name.firstName();
     this.location = {
       lat: parseFloat(faker_1.faker.address.latitude()),
@@ -22808,6 +22809,7 @@ exports.Company = void 0;
 var faker_1 = require("@faker-js/faker");
 var Company = /** @class */function () {
   function Company() {
+    this.color = "red";
     this.companyName = faker_1.faker.company.name();
     this.catchPhrase = faker_1.faker.company.catchPhrase();
     this.location = {
