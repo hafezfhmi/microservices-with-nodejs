b1fddd52116a23139d93d4d7b0c772ff83625518
hafezfhmi <hafezfahmi2000@gmail.com>
2023-03-28 15:43:09 +0800

    b.069 Showing Popup Windows

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -22833,12 +22833,19 @@ var CustomMap = /** @class */function () {
     });
   }
   CustomMap.prototype.addMarker = function (mappable) {
-    new google.maps.Marker({
+    var _this = this;
+    var marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+    marker.addListener("click", function () {
+      var infoWindow = new google.maps.InfoWindow({
+        content: "Hi there!"
+      });
+      infoWindow.open(_this.googleMap, marker);
     });
   };
   return CustomMap;
