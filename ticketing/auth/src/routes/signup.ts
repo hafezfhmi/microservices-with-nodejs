843fb072dc043d908c1ae3cd9f10ccbbb9e4ecb4
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-05 16:20:23 +0800

    9.014 Accessing Env Variables in a Pod

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -36,7 +36,10 @@ router.post(
     await user.save();
 
     // Generate JWT
-    const userJwt = jwt.sign({ id: user.id, email: user.email }, "asdf");
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
 
     // Store it on session object
     req.session = { jwt: userJwt };
