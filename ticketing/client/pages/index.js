cc83805a4af5eb35d310f204da1b11c64417b3fc
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-14 02:09:59 +0800

    11.016 Overview on Server Side Rendering

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Index = () => {
+const Index = ({ color }) => {
+  console.log("I am in the component", color);
   return <div>Landing Page</div>;
+};
+
+Index.getInitialProps = () => {
+  console.log("I am on the server");
+
+  return { color: "red" };
 };
 
 export default Index;
