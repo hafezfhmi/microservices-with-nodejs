145fee15794f54c84da42a9e88dd88b716c08ecd
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-14 14:58:19 +0800

    11.025 Specifying the Host

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import axios from "axios";
 
 const Index = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <div>Landing Page</div>;
 };
 
 Index.getInitialProps = async () => {
   if (typeof window === "undefined") {
-    // we are on the server!
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return data;
   } else {
-    // we are on the browser!
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
   }
 
   return {};
