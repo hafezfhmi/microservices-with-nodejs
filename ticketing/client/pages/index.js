4c0edd592fd5e74dc1c0bd53b70e1f4136323d91
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-14 18:10:37 +0800

    11.026 Passing Through the Cookies

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -7,14 +7,12 @@ const Index = ({ currentUser }) => {
   return <div>Landing Page</div>;
 };
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async ({ req }) => {
   if (typeof window === "undefined") {
     const { data } = await axios.get(
       "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
       {
-        headers: {
-          Host: "ticketing.dev",
-        },
+        headers: req.headers,
       }
     );
     return data;
