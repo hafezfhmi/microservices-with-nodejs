31727310354696e6b3c9d1b4bf8fee8c4fead3d5
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-14 19:10:17 +0800

    11.028 Content on the Landing Page

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import buildClient from "../api/build-client";
 
 const Index = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return <div>Landing Page</div>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT singed in</h1>
+  );
 };
 
 Index.getInitialProps = async (context) => {
