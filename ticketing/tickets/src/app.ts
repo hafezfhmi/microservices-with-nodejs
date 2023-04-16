f8e7f95d133c6aea584deaffcb1198ca671e4dcb
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-16 23:07:07 +0800

    13.008 Creating the Router

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@hafezfhmi-dev/common";
 
+import { createTicketRouter } from "./routes/__test__/new";
+
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -13,6 +15,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(createTicketRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
