723bcf7f6fff0667aad8f2e590275ce9cef8839b
hafezfhmi <hafezfahmi2000@gmail.com>
2023-04-18 17:10:57 +0800

    13.020 Better Error Logging

--- ticketing/tickets/src/routes/__test__/show.test.ts
+++ ticketing/tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,11 @@
 import request from "supertest";
+import mongoose from "mongoose";
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
-  await request(app)
-    .get("/api/tickets/dsadsadasfsadsadsaaaa")
-    .send()
-    .expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns a ticket if the ticket is found", async () => {
