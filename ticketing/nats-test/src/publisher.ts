7d0209df686abe5247257ec413110a7af37f543d
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-13 22:33:00 +0800

    15.014 Awaiting Event Publication

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -7,7 +7,7 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
   // const data = JSON.stringify({
@@ -21,9 +21,13 @@ stan.on("connect", () => {
   // });
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
