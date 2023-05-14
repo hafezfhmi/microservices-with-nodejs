c9b74750a96e86b396fbbb1cfa1bc0cd53cabb87
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-14 18:35:14 +0800

    16.006 Singleton Implementation

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -25,12 +25,12 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
-    new TicketCreatedPublisher(client).publish({
-      id: ticket.id,
-      title: ticket.title,
-      price: ticket.price,
-      userId: ticket.userId,
-    });
+    // new TicketCreatedPublisher(client).publish({
+    //   id: ticket.id,
+    //   title: ticket.title,
+    //   price: ticket.price,
+    //   userId: ticket.userId,
+    // });
 
     res.status(201).send(ticket);
   }
