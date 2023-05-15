648fadf0b8d9937925c4c7f14d03f5c72b5f9abe
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-15 22:12:01 +0800

    16.017 Ensuring Mock Invocations

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -74,4 +75,18 @@ it("creates a ticket with a valid inputs", async () => {
 
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
+});
+
+it("publishes an event", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "dsadsadas",
+      price: 20,
+    })
+    .expect(201);
+
+  // console.log(natsWrapper);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
