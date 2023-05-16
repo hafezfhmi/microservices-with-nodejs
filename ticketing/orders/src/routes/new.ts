4cac5f7bda9e47a62388c6c59f5d253d5899be4c
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-16 22:47:50 +0800

    17.006 Subtle Service Coupling

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { requireAuth, validateRequest } from "@hafezfhmi-dev/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticket")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
