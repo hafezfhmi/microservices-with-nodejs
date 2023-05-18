baf6d732686811afd6256b4e60f347b24a537137
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-18 12:08:12 +0800

    17.021 Asserting Reserved Tickets

--- ticketing/orders/src/routes/__test__/new.test.ts
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -14,25 +14,25 @@ it("returns an error if the ticket does not exist", async () => {
     .expect(404);
 });
 
-// it("returns an error if the ticket is already reserved", async () => {
-//   const ticket = Ticket.build({
-//     title: "concert",
-//     price: 20,
-//   });
-//   await ticket.save();
-//   const order = Order.build({
-//     ticket,
-//     userId: "32132131",
-//     status: OrderStatus.Created,
-//     expiresAt: new Date(),
-//   });
-//   await order.save();
+it("returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    ticket,
+    userId: "32132131",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
 
-//   await request(app)
-//     .post("/api/orders")
-//     .set("Cookie", global.signin())
-//     .send({ ticketId: ticket.id })
-//     .expect(400);
-// });
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
 
 it("reserves a ticket", async () => {});
