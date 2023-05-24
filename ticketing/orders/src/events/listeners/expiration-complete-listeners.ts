34359170d3d160e4dc81f6634129254ae6ab02b8
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-24 21:49:47 +0800

    20.016 Emitting the Order Cancelled Event

--- ticketing/orders/src/events/listeners/expiration-complete-listeners.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listeners.ts
@@ -7,13 +7,15 @@ import {
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: ExpirationCompleteEvent["data"], message: Message) {
-    const order = await Order.findById(data.orderId);
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
       throw new Error("Order not found");
@@ -22,5 +24,15 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({
       status: OrderStatus.Cancelled,
     });
+    await order.save();
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
   }
 }
