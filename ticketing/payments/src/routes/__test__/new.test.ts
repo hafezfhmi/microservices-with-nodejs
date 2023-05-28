0dcdd18438588c572a60df3a415efd9a0a9f85c8
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-28 17:59:49 +0800

    21.022 Mocked Stripe Client

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { OrderStatus } from "@hafezfhmi-dev/common";
 import { app } from "../../app";
 import { Order } from "../../models/order";
+import { stripe } from "../../stripe";
 
 jest.mock("../../stripe");
 
@@ -78,5 +79,10 @@ it("returns a 204 with valid inputs", async () => {
       token: "tok_visa",
       orderId: order.id,
     })
-    .expect(204);
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual("tok_visa");
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual("myr");
 });
