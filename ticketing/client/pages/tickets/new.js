15e5e6a3a909429ca578e5df8914adb4e6f855a3
hafezfhmi <hafezfahmi2000@gmail.com>
2023-05-29 22:16:28 +0800

    22.004 Scaffolding a Form

---
+++ ticketing/client/pages/tickets/new.js
@@ -0,0 +1,20 @@
+const NewTicket = () => {
+  return (
+    <div>
+      <h1>Create a Ticket</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Price</label>
+          <input className="form-control" type="text" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTicket;
